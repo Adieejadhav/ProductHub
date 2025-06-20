5ac094a68dc49ddc8be68648646c49272894ad7e
Adieejadhav <adijadhav504@gmail.com>
2025-06-21 00:37:21 +0530

    TRIGGERING THE REDEPLOYE

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Trigger redeploy - no functional change
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddItem from './pages/AddItem';
